4cd35d17ec97ba0dabe046694bf3fcfaf68d2d34
Hendri Wahyu Perdana <hendriwp123@gmail.com>
2024-11-07 22:55:14 +0700

    ♻️  fix: dashboard bookshelf

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('admin.index');
+        $countBooks = Book::count();
+        $countCategories = Category::count();
+
+        $books = Book::latest('created_at')->take(3)->get();
+
+        return view('admin.index', compact('books', 'countBooks', 'countCategories'));
     }
 }
