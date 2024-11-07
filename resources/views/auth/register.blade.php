3618e832e75978fe70c0478b3a0e736d82d43a11
Hendri Wahyu Perdana <hendriwp123@gmail.com>
2024-11-07 23:06:32 +0700

    ♻️  fix: register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,35 +16,47 @@
                             <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                         </div>
 
-                        <!-- Notification for Errors -->
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul class="mb-0">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
                         <form class="user" method="POST" action={{route('registerUser')}}>
                             @csrf
                             <div class="form-group">
-                                <input type="text" class="form-control form-control-user" id="exampleFirstName"
+                                <input type="text" class="form-control form-control-user @error('name')
+                                is-invalid
+                                @enderror" id="exampleFirstName"
                                     placeholder="Full Name" value="{{ old('name') }}" name="name">
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group">
-                                <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                    placeholder="Email Address" value="{{ old('email') }}" name="name">
+                                <input type="email" class="form-control form-control-user @error('email')
+                                is-invalid
+                                @enderror" id="exampleInputEmail"
+                                    placeholder="Email Address" value="{{ old('email') }}" name="email">
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input type="password" class="form-control form-control-user"
+                                    <input type="password" class="form-control form-control-user @error('password')
+                                    is-invalid
+                                    @enderror"
                                         id="exampleInputPassword" placeholder="Password" name="password">
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
                                 <div class="col-sm-6">
-                                    <input type="password" class="form-control form-control-user"
-                                        id="exampleRepeatPassword" placeholder="Confirm Password" name="confirm_password">
+                                    <input type="password" class="form-control form-control-user @error('password')
+                                    is-invalid
+                                    @enderror"
+                                        id="exampleRepeatPassword" placeholder="Confirm Password" name="password_confirmation">
                                 </div>
                             </div>
                             <button type="submit" href={{route('registerUser')}} class="btn btn-primary btn-user btn-block">
